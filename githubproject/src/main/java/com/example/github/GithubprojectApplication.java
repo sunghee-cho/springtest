5664c4f0de1972801dd85e25ee5ac6609f0ce834
SAMSUNG <bluejeansh@hanmail.net>
2023-12-27 17:47:16 +0900

    gitignore 수정

--- githubproject/src/main/java/com/example/github/GithubprojectApplication.java
+++ githubproject/src/main/java/com/example/github/GithubprojectApplication.java
@@ -8,6 +8,7 @@ public class GithubprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubprojectApplication.class, args);
+		System.out.println("github push 이후 추가합니다");
 	}
 
 }
