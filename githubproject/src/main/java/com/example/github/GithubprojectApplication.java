ac96530637b15353ae5903f0a5cd133598f0f09d
SAMSUNG <bluejeansh@hanmail.net>
2023-12-28 16:39:09 +0900

    sub1 브랜치 commit

--- githubproject/src/main/java/com/example/github/GithubprojectApplication.java
+++ githubproject/src/main/java/com/example/github/GithubprojectApplication.java
@@ -9,6 +9,7 @@ public class GithubprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GithubprojectApplication.class, args);
 		System.out.println("github push 이후 추가합니다");
+		System.out.println("sub1 브랜치에 github push 이후 추가합니다");
 	}
 
 }
